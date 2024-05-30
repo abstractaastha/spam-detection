8a93f44f079a6dffa62660aef4049b285ac01c7e
Aastha <100476961+abstractaastha@users.noreply.github.com>
2024-05-31 03:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-#nltk.download('punkt')
-#nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 
 # Initialize the Porter Stemmer
